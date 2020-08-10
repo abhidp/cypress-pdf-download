71e8c7c8c23be46e2c7418c035acb012105c501d
abhidp <abhi358@gmail.com>
2020-08-10 19:52:52 +1000

    working copy

--- cypress/integration/pdfExport.spec.js
+++ cypress/integration/pdfExport.spec.js
@@ -1,6 +1,6 @@
 describe('Validate PDF export feature', () => {
   before('Clear downloads folder', () => {
-    cy.exec('rm cypress/downloads/*', { log: true, failOnNonZeroExit: false });
+    cy.exec('rm cypress/downloads/SlypReceipt.pdf', { log: true, failOnNonZeroExit: false });
   });
 
   it('Should open action modal when clicked on kebab menu', () => {
@@ -8,5 +8,35 @@ describe('Validate PDF export feature', () => {
     cy.get('ExportPdfButton').should('be.visible');
     cy.get('ExportPdfButton').scrollIntoView().click();
     cy.task('isExistPDF', 'SlypReceipt.pdf').should('equal', true);
+  });
+
+  it('Should validate the contents of the PDF', () => {
+    cy
+      .task('getPDFContent', 'SlypReceipt_Working.pdf')
+      .should(
+        'include.members',
+        [
+          'General Pants',
+          '107 Elizabeth Street',
+          'Sydney, NSW, 2000',
+          'ABN: 81 071 213 682',
+          'Tax Invoice',
+          '$518.90',
+          'The Opposite Mesh Top White$139.95',
+          'Hi & Wasted Jean The Streets Blue$179.95',
+          'Clyde Heel Black Velvet$199.00',
+          'Subtotal(3)$471.73',
+          'Total$518.90',
+          'Tax included in total$47.17',
+          'Purchased on 19/07/2019, 14:34',
+          'Return expires on 17/09/2019',
+          'Return Policy',
+          'This receipt can be used for exchange or refund.',
+          '123456789012',
+          'Thank you!',
+          'Powered by',
+        ],
+        'PDF Corrupted'
+      );
   });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,6 +6,7 @@
 
 const path = require('path');
 const fs = require('fs');
+const pdfParse = require('pdf-parse');
 const downloadDirectory = path.join(__dirname, '..', 'downloads');
 
 const findPDF = (PDFfilename) => {
@@ -30,11 +31,43 @@ const hasPDF = (PDFfilename, ms) => {
   });
 };
 
+const parsePDF = (PDFfilename) => {
+  const PDFfile = `${downloadDirectory}/${PDFfilename}`;
+  const dataBuffer = fs.readFileSync(PDFfile);
+  let textArray = [];
+  return new Promise((resolve, reject) => {
+    const data = pdfParse(dataBuffer)
+      .then((data) => {
+        return resolve(data.text.split('\n').filter(Boolean));
+      })
+      .catch((error) => {
+        return reject(error);
+      });
+  });
+};
+
 module.exports = (on, config) => {
+  on('before:browser:launch', (browser, options) => {
+    if (browser.family === 'chromium') {
+      options.preferences.default['download'] = {
+        default_directory: downloadDirectory,
+      };
+      return options;
+    }
+    if (browser.family === 'firefox') {
+      options.preferences['browser.download.dir'] = downloadDirectory;
+      options.preferences['browser.download.folderList'] = 2;
+      options.preferences['browser.helperApps.neverAsk.saveToDisk'] = 'text/csv';
+      return options;
+    }
+  });
   on('task', {
     isExistPDF(PDFfilename, ms = 5000) {
       console.log(`looking for PDF file in ${downloadDirectory}`, PDFfilename, ms);
       return hasPDF(PDFfilename, ms);
+    },
+    getPDFContent(PDFfilename) {
+      return parsePDF(PDFfilename);
     },
   });
 

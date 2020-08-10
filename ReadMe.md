a5dd288552867f908d2cebb1c957cc6ace5886a0
abhidp <abhi358@gmail.com>
2020-08-10 21:04:29 +1000

    updated ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,29 @@
+This is a reproducible source code for the [Issue #8232](https://github.com/cypress-io/cypress/issues/8232) raised with Cypress
+
+Instructions:
+- clone this repo : `git clone https://github.com/abhidp/cypress-pdf-download.git`
+- install dependencies: `npm install`
+- open Cypress runner : `npm run cy:open`
+- execute test : `pdfExport.spec.js`
+- `Test#1` and `Test#3` will pass
+- `Test#2` will fail because the downloaded PDF file is blank which is what this issue is all about
+
+Tests will run against the url: https://receipts.uat-slyp.com.au/WRA-ba8511d6f86fcd488cc335a356ccadd2d37f1c30
+
+Problem: 
+
+When the above steps are executed in Cypress, it downloads a blank PDF file:  `/cypress/downloads/SlypReceipt.pdf`
+When manually downloaded, you get the correct PDF file: `/cypress/downloads/SlypReceipt_Working.pdf`
+
+Actual Result: Cypress downloads a blank PDF
+Expected Result: Cypress should download the same PDF as downloaded manually 
+
+Replication steps to download the PDF manually:
+- navigate to the above url
+- click on the Kebab menu (three vertical dots) at the top right corner of the page
+- click on `Export as PDF` option and save it to your local disk
+
+
+| PDF downloaded by Cypress   |   PDF downloaded manually |
+| :---: | :---: |
+| ![](cypress/downloads/PDF_downloaded_by_Cypress.jpg?raw=true "PDF downloaded by Cypress") | ![](cypress/downloads/PDF_downloaded_manually.jpg?raw=true "PDF downloaded by Cypress") |
--- cypress/integration/pdfExport.spec.js
+++ cypress/integration/pdfExport.spec.js
@@ -1,16 +1,46 @@
 describe('Validate PDF export feature', () => {
-  before('Clear downloads folder', () => {
+  before('Clear old SlypReceipt.pdf files from downloads folder', () => {
     cy.exec('rm cypress/downloads/SlypReceipt.pdf', { log: true, failOnNonZeroExit: false });
   });
 
-  it('Should open action modal when clicked on kebab menu', () => {
+  it('Test#1 Should open action modal when clicked on kebab menu', () => {
     cy.get('KebabMenu').should('be.visible').click();
     cy.get('ExportPdfButton').should('be.visible');
     cy.get('ExportPdfButton').scrollIntoView().click();
     cy.task('isExistPDF', 'SlypReceipt.pdf').should('equal', true);
   });
 
-  it('Should validate the contents of the PDF', () => {
+  it('Test#2 ACTUAL RESULT -- VALIDATE CONTENTS OF PDF DOWNLOADED BY CYPRESS (SlypReceipt.pdf)', () => {
+    cy
+      .task('getPDFContent', 'SlypReceipt.pdf')
+      .should(
+        'include.members',
+        [
+          'General Pants',
+          '107 Elizabeth Street',
+          'Sydney, NSW, 2000',
+          'ABN: 81 071 213 682',
+          'Tax Invoice',
+          '$518.90',
+          'The Opposite Mesh Top White$139.95',
+          'Hi & Wasted Jean The Streets Blue$179.95',
+          'Clyde Heel Black Velvet$199.00',
+          'Subtotal(3)$471.73',
+          'Total$518.90',
+          'Tax included in total$47.17',
+          'Purchased on 19/07/2019, 14:34',
+          'Return expires on 17/09/2019',
+          'Return Policy',
+          'This receipt can be used for exchange or refund.',
+          '123456789012',
+          'Thank you!',
+          'Powered by',
+        ],
+        'PDF Contents Validation'
+      );
+  });
+
+  it('Test#3 EXPECTED RESULT -- VALIDATE CONTENTS OF PDF DOWNLOADED MANUALLY (SlypReceipt_Working.pdf)', () => {
     cy
       .task('getPDFContent', 'SlypReceipt_Working.pdf')
       .should(
@@ -36,7 +66,7 @@ describe('Validate PDF export feature', () => {
           'Thank you!',
           'Powered by',
         ],
-        'PDF Corrupted'
+        'PDF Contents Validation'
       );
   });
 });
